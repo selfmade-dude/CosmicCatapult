243a7e9fc39cf2a95547ca3783aeb72c5c454d52
Joe <joe.bokun@icloud.com>
2025-11-16 23:20:58 +0300

    Add OrbitState struct for orbital parameters

---
+++ core/OrbitState.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Vector2.h"
+
+// Represents the current orbital state of a spacecraft/particle.
+// No physics logic is implemented here yet - this is a pure data container.
+
+struct OrbitState
+{
+    Vector2 position;      // [m]
+    Vector2 velocity;      // [m/s]
+
+    double radius = 0.0;   // |r| [m]
+    double speed = 0.0;    // |v| [m/s]
+
+    double energy = 0.0;   // specific orbital energy [m^2/s^2]
+
+    double eccentricity = 0.0;  // dimensionless
+    double trueAnomaly = 0.0;   // [rad]
+
+    // This will be expanded later when we add semimajor axis, periapsis distance, etc.
+}
