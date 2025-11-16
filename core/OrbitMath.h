ca6aff62e5890365f5c39fa9db3cd0bda23c8941
Joe <joe.bokun@icloud.com>
2025-11-16 23:44:05 +0300

    Add OrbitMath helpers for radius and speed from Vector2

---
+++ core/OrbitMath.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cmath>
+#include "Vector2.h"
+
+// Basic orbital helper funcitons that operate on Vector2.
+// No orbital formulas here yet, only simple vector-based quantities.
+inline double radiusFromPosition(const Vector2 &position)
+{
+    return std::sqrt(position.x * position.x + positon.y * position.y)
+}
+
+inline double speedFromVelocity(const Vector2 &velocity)
+{
+    return std::sqrt(velocity.x * velocity.x + velocity.y * velocity.y)
+}
