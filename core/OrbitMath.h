46d86968c670e18ea880cec6c0425a9d81a32178
Joe <joe.bokun@icloud.com>
2025-11-17 22:06:00 +0300

    Add cosBetween helper using dot product and magnitudes

--- core/OrbitMath.h
+++ core/OrbitMath.h
@@ -15,7 +15,20 @@ inline double speedFromVelocity(const Vector2 &velocity)
     return std::sqrt(velocity.x * velocity.x + velocity.y * velocity.y)
 }
 
-inline double dot(const Vector2 &a, const Vector2 &b)
+inline double dot(const Vector2 &a, const Vector2 &b) // Dot product
 {
     return a.x * b.x + a.y * b.y;
+}
+
+inline double cosBetween(const Vector2 &a, const Vector2 &b) // Cosine of angle between vectors
+{
+    const double magA = std::sqrt(a.x * a.x + a.y * a.y);
+    const double magB = std::sqrt(b.x * b.x + b.y * b.y);
+
+    if (magA == 0.0 || magB == 0.0)
+    {
+        return 0.0;
+    }
+
+    return dot(a, b) / (magA * magB);
 }
