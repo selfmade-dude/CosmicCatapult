9ceca54ba310e93451b328ebe2b38ca070005546
Joe <joe.bokun@icloud.com>
2025-11-22 19:37:38 +0300

    Add console sanity-check to main.cpp using orbitToStateString

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,22 @@
+#include <iostream>
 #include <QApplication>
 #include "MainWindow.h"
+#include "OrbitUtils.h"
+#include "OrbitDebug.h"
 
 // Include the core header (test!)
 #include "Body.h"
 
 int main(int argc, char *argv[])
 {
+    Vector2 position(7000.0, 0.0);
+    Vector2 velocity(0.0, 7.5);
+    double mu = 398600.0;
+
+    OrbitState st = makeOrbitState(position, velocity, mu);
+
+    std::cout << orbitStateToString(st) << std::endl;
+
     QApplication app(argc, argv);
 
     MainWindow window;
