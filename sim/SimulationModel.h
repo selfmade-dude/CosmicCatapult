72548b06f249999b5efec1a32e161dd758b07db1
Joe <joe.bokun@icloud.com>
2025-12-08 21:46:48 +0300

    Add SimulationModel combining controller, clock, trajectory

---
+++ sim/SimulationModel.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "SimulationController.h"
+#include "SimulationClock.h"
+#include "TrajectoryBuffer.h"
+
+class SimulaionModel
+{
+public:
+    SimulationModel(const State2 &initialState, double muValue, double dtValue, IntegratorType integratorType = IntegratorType::RK4, std::size_t trajectoryMaxSize = 5000) : controller_(initialState, muValue, dtValue, integratorType), clock_(0.0), trajectory_(trajectoryMaxSize)
+    {
+        trajectory_.addPoint(initialState.position);
+    }
+
+    void update()
+    {
+        controller_.step();
+        clock_.advance(controller_.state().dt());
+
+        clock_.advance(dt());
+        trajectory_.addPoint(controller_.state().position);
+    }
+
+    void reset(const State2 &newState)
+    {
+        controller_.reset(newState);
+        clock_.reset(0.0);
+        trajectory_.clear();
+        trajectroy_.addPoint(newState.position);
+    }
+
+    const State2& state() const
+    {
+        return controller_.state();
+    }
+
+    double time() const
+    {
+        return clock_.time();
+    }
+
+    const std::vector<Vector2>& trajectory() const
+    {
+        return trajectory_.points();
+    }
+
+    double dt() const
+    {
+        return controllerDt_;
+    }
+
+    void setDt(double newDt)
+    {
+        controller_.setDt(newDt);
+        controllerDt_ = newDt;
+    }
+
+    void setMu(double newMu)
+    {
+        controller_.setMu(newMu);
+    }
+
+    void setIntegrator(IntegratorType type)
+    {
+        controller_.setIntegrator(type);
+    }
+
+private:
+    SimulationController controller_;
+    SimulationClock clock_;
+    TrajectoryBuffer trajectory_;
+    double controllerDt_ = 0.0;
+};
