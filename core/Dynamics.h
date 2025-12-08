bee9221aed7063678bd1d6f1afe2deb18a1cc4c9
Joe <joe.bokun@icloud.com>
2025-12-08 19:45:16 +0300

    Add derivatives() returning (velocity, gravitational acceleration)

--- core/Dynamics.h
+++ core/Dynamics.h
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include "OrbitMath.h"
+#include "State2D.h"
 #include "Vector2.h"
 
 // Compute gravitational acceleration at a given position
@@ -18,4 +19,15 @@ inline Vector2 gravitationalAcceleration(const Vector2 &position, double mu)
     const double factor = -mu / (r * r * r);
 
     return Vector2(factor * position.x, factor * position.y);
+}
+
+inline State2D derivatives(const State2D &state, double mu)
+{
+    State2D result;
+
+    result.position = state.velocity;
+
+    result.velocity = gravitationalAcceleration(state.position, mu);
+
+    return result;
 }
