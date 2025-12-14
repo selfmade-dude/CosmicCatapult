8cf006303f063c366d8603a89f145c350157c8b3
Joe <joe.bokun@icloud.com>
2025-12-14 17:18:23 +0300

    Add ScenarioParams struct for simulation reset

---
+++ sim/ScenarioParams.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Vector2.h"
+
+struct ScenarioParams
+{
+    Vector2 shipPosition = Vector2(0.0, 0.0);
+    Vector2 shipVelocity = Vector2(0.0, 0.0);
+
+    double dt = 0.1;
+
+    bool clearTrajectoriesOnReset = true;
+};
